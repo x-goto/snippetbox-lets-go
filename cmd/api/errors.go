4c6b581c38df178a28d9e083db32f2d91d9bafe1
zhapalaqq <sakkuna000@gmail.com>
2023-03-08 02:42:41 +0600

    chapter 5 some fixes, cause i had errors

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -14,16 +14,12 @@ func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Reque
 	app.errorResponse(w, r, err, "internal server error", http.StatusInternalServerError)
 }
 
-func (app *application) userUnauthorizedResponse(w http.ResponseWriter, r *http.Request) {
-	app.errorResponse(w, r, errors.New("user unauthorized"), "user unauthorized error", http.StatusUnauthorized)
-}
-
 func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
 	app.errorResponse(w, r, errors.New("resource not found"), "resource not found", http.StatusNotFound)
 }
 
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request) {
-	app.errorResponse(w, r, errors.New("Bad Request"), "bad request", http.StatusBadRequest)
+	app.errorResponse(w, r, errors.New("bad request"), "bad request", http.StatusBadRequest)
 }
 
 func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,8 +2,6 @@ package main
 
 import "net/http"
 
-type envelope map[string]any
-
 func (app *application) logError(r *http.Request, err error) {
 	app.logger.PrintError(err, map[string]string{
 		"request_method": r.Method,
